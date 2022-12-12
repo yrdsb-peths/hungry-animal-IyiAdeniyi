6e0c8cfe5ac6588c5b14728cd788b6c95f37d2d8
IyiAdeniyi <340925593@gapps.yrdsb.ca>
2022-12-11 22:40:11 -0500

    animate elephant

--- Elephant.java
+++ Elephant.java
@@ -13,18 +13,42 @@ public class Elephant extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     
-    GreenfootSound elephantSound = new GreenfootSound("");
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage idle[] = new GreenfootImage[8];
+    
+    public Elephant()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle"+ i + ".png");
+        }
+        
+        setImage(idle[0]);
+    }
+    
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
         {
-            move(-1);
+            move(-2);
         }
         else if(Greenfoot.isKeyDown("right"))
         {
-            move(1);
+            move(2);
         }
         eat();
+        
+        
+        //Animate the elphant
+        
+        animateElephant();
     }
     
     public void eat()
@@ -35,6 +59,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
             world.increaseScore();
+            elephantSound.play();
         }
 
     }
